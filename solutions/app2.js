765e1d2ab44906bfee91a1f86e48e1e7254af0c5
Mónika <gergye.monika@gmail.com>
2022-08-21 22:33:29 +0200

    negyedik gyakorlás

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,15 @@
 
+function countOfWorkingDays(){
+    const date1= new Date('2022-08-31T00:00:00');
+    const date2= new Date('2022-01-01T00:00:00');
+
+
+    return Math.ceil(((date1.getTime()-date2.getTime())/1000/60/60/24/7)*5)
+}
+console.log(countOfWorkingDays())
+
+//export { countOfWorkingDays}
 
 
 
-
-
-// export default countOfWorkingDays;
+ export default countOfWorkingDays;
